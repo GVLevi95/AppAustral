4cac2ea91a1d36e4a01919625b417d8c34f9506e
KevinJordanCA <55149729+KevinJordanCA@users.noreply.github.com>
2019-10-01 17:26:23 -0300

    marcadores

--- app/src/main/java/com/frunaos/appaustral/MapsActivity.java
+++ app/src/main/java/com/frunaos/appaustral/MapsActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -84,6 +85,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Marcadores
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+
+        LatLng pelluco = new LatLng(-41.489681, -72.893700);
+        MarkerOptions markerOptionsPelluco =
+                new MarkerOptions()
+                        .position(pelluco)
+                        .title("Pelluco")
+                        .snippet("Primera parada de la Carreta Austral");
+        Marker pelluco_marker = googleMap.addMarker(markerOptionsPelluco);
+
+
+        LatLng pelluhuin = new LatLng(-41.489681, -72.893700);
+        MarkerOptions markerOptionsPelluhuin =
+                new MarkerOptions()
+                        .position(pelluhuin)
+                        .title("Pelluhuin")
+                        .snippet("Primera parada de la Carreta Austral");
+        Marker pelluhuin_marker = googleMap.addMarker(markerOptionsPelluhuin);
+
+
+        LatLng coihuin = new LatLng(-41.486304, -72.861439);
+        MarkerOptions markerOptionsCoihuin =
+                new MarkerOptions()
+                        .position(coihuin)
+                        .title("Coihuin")
+                        .snippet("Tercera parada de la Carreta Austral");
+        Marker coihuin_marker = googleMap.addMarker(markerOptionsCoihuin);
+
+        LatLng chamiza = new LatLng(-41.482950, -72.845179);
+        MarkerOptions markerOptionsChamiza =
+                new MarkerOptions()
+                        .position(chamiza)
+                        .title("Chamiza")
+                        .snippet("Cuarta parada de la Carreta Austral");
+        Marker chamiza_marker = googleMap.addMarker(markerOptionsChamiza);
+
+        LatLng blue_rock = new LatLng(-41.503688, -72.800114);
+        MarkerOptions markerOptionsBR =
+                new MarkerOptions()
+                        .position(blue_rock)
+                        .title("Piedra Azul")
+                        .snippet("Quinta parada de la Carreta Austral");
+        Marker br_marker = googleMap.addMarker(markerOptionsBR);
+
+        LatLng pichiquillaipe = new LatLng(-41.522128, -72.763744);
+        MarkerOptions markerOptionsPichiquillaipe =
+                new MarkerOptions()
+                        .position(pichiquillaipe)
+                        .title("")
+                        .snippet("Sexta parada de la Carreta Austral");
+        Marker pichiquillaipe_marker = googleMap.addMarker(markerOptionsPichiquillaipe);
+
+        LatLng quillaipe = new LatLng(-41.527532, -72.745565);
+        MarkerOptions markerOptionsQuillaipe =
+                new MarkerOptions()
+                        .position(quillaipe)
+                        .title("Quillaipe")
+                        .snippet("Séptima parada de la Carreta Austral");
+        Marker q_marker = googleMap.addMarker(markerOptionsQuillaipe);
+
+        LatLng metri = new LatLng(-41.592477, -72.702301);
+        MarkerOptions markerOptionsMetri =
+                new MarkerOptions()
+                        .position(metri)
+                        .title("Metri")
+                        .snippet("Octava parada de la Carreta Austral");
+        Marker metri_marker = googleMap.addMarker(markerOptionsMetri);
+
+        LatLng lenca = new LatLng(-41.606301, -72.683388);
+        MarkerOptions markerOptionsLenca =
+                new MarkerOptions()
+                        .position(lenca)
+                        .title("Lenca")
+                        .snippet("Novena parada de la Carreta Austral");
+        Marker lenca_marker = googleMap.addMarker(markerOptionsLenca);
+
+        LatLng chaicas = new LatLng(-41.615210, -72.658393);
+        MarkerOptions markerOptionsChaicas =
+                new MarkerOptions()
+                        .position(chaicas)
+                        .title("Chaicas")
+                        .snippet("Décima parada de la Carreta Austral");
+        Marker chaicas_marker = googleMap.addMarker(markerOptionsChaicas);
+
+        LatLng cl_gutierrez = new LatLng(-41.660768, -72.663512);
+        MarkerOptions markerOptionsCl_gutierrez =
+                new MarkerOptions()
+                        .position(cl_gutierrez)
+                        .title("Caleta Gutierrez")
+                        .snippet("Onceava parada de la Carreta Austral");
+        Marker cl_gutierrez_marker = googleMap.addMarker(markerOptionsCl_gutierrez);
+
+        LatLng cl_la_arena = new LatLng(-41.691045, -72.638697);
+        MarkerOptions markerOptionsCL_Arena =
+                new MarkerOptions()
+                        .position(cl_la_arena)
+                        .title("Caleta la Arena")
+                        .snippet("Doceava parada de la Carreta Austral");
+        Marker cl_la_arena_marker = googleMap.addMarker(markerOptionsCL_Arena);
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
