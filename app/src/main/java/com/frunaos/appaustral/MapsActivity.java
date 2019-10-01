51f233874b347e95132db4d07ffecf6be4aa33ff
Pedro <55149673+Deckerdyn@users.noreply.github.com>
2019-10-01 12:29:27 -0300

    prueba
    commit de prueba

--- app/src/main/java/com/frunaos/appaustral/MapsActivity.java
+++ app/src/main/java/com/frunaos/appaustral/MapsActivity.java
@@ -36,7 +36,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
     }
-
+    //prueba
 
     /**
      * Manipulates the map once available.
