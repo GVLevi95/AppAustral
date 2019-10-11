44f01ac48cd2269cf1095696f99e4924f12473be
GVLevi95 <55149703+GVLevi95@users.noreply.github.com>
2019-10-11 11:09:57 -0300

    a

--- app/src/main/java/com/frunaos/appaustral/ui/Municipal/MunicipalFragment.java
+++ app/src/main/java/com/frunaos/appaustral/ui/Municipal/MunicipalFragment.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.frunaos.appaustral.R;
 
@@ -19,7 +23,17 @@ public class MunicipalFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.municipal_fragment, container, false);
+        mViewModel =
+                ViewModelProviders.of(this).get(MunicipalViewModel.class);
+        View root = inflater.inflate(R.layout.municipal_fragment, container, false);
+        final TextView textView = root.findViewById(R.id.text_send);
+        mViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 
 }
